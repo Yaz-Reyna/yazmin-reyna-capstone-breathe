65806c46e8549c6afc3c79885fab19e3cdf99b74
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-06 12:24:25 -0500

    Add color variables. Add home background

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,17 +5,21 @@ function HomePage() {
 
     return(
         <main className='main'>
-            <div className='main__container'>
-                <h1 className='main__container__title'>BREATHE</h1>
-                <div className='main__container__links-container'>
-                    <NavLink className='main__container__links-container--link'>
+            
+                <h1 className='main__title'>BREATHE</h1>
+                <div className='main__links-container'>
+                    <NavLink className='main__links-container--link'>
+                        Relaxing Music
+                    </NavLink>
+                    <NavLink className='main__links-container--link'>
                         Breathing Exercises
                     </NavLink>
-                    <NavLink className='main__container__links-container--link'>
+                    <NavLink className='main__links-container--link'>
                         Words of Encouragement
                     </NavLink>
+                    
                 </div>
-            </div>
+           
         </main>
     )
 }
