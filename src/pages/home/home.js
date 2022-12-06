b26dd34986aa91dcce195ee587a3ac69ea64a177
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-05 19:57:53 -0500

    Add home structure

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,23 @@
 import './home.scss';
+import { NavLink } from "react-router-dom";
 
 function HomePage() {
 
     return(
-        <main>
-            <div>
-                <h1>BREATHE</h1>
-                <div>
-                    
+        <main className='main'>
+            <div className='main__container'>
+                <h1 className='main__container__title'>BREATHE</h1>
+                <div className='main__container__links-container'>
+                    <NavLink className='main__container__links-container--link'>
+                        Breathing Exercises
+                    </NavLink>
+                    <NavLink className='main__container__links-container--link'>
+                        Words of Encouragement
+                    </NavLink>
                 </div>
             </div>
         </main>
     )
 }
+
+export default HomePage;
