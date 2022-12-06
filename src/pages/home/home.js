553da6a0e3b753a1cc0909a5241d1cbcaece0260
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-05 19:49:19 -0500

    AAdd media queries to mixins. Install sass, axios, react router dom

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import './home.scss';
+
+function HomePage() {
+
+    return(
+        <main>
+            <div>
+                <h1>BREATHE</h1>
+                <div>
+                    
+                </div>
+            </div>
+        </main>
+    )
+}
