e9bf65f06095bffd897b7c7eacfbc9827c7c5ce5
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-10 17:08:04 -0500

    Add form component and style

--- src/components/CommentsForm/CommentsForm.js
+++ src/components/CommentsForm/CommentsForm.js
@@ -4,16 +4,21 @@ import './CommentsForm.scss';
 
 function CommentsForm() {
     return (
-        <form className='qj-form' action="submit">
-            <label className='qj-form__label' htmlFor="name">
-                Name
-                <input className='qj-form__input qj-form__input--name'  type="text" placeholder='Enter your name here' name='name' />
-            </label>
-            <label className='qj-form__label' htmlFor="comment">
-                Comment 
-                <textarea className='qj-form__input qj-form__input--comment' name="comment" placeholder='Write your comment here'></textarea>
-            </label>
-        </form>
+        <section className='comments'>
+            <h2 className='comments__title'>Leave us a comment</h2>
+            <form className='comments__qj-form' action="submit">
+                <label className='comments__qj-form--label' htmlFor="name">
+                    Name
+                    <input className='comments__qj-form__input comments__qj-form__input--name '  type="text" placeholder='Enter your name here' name='name' />
+                </label>
+                <label className='comments__qj-form--label' htmlFor="comment">
+                    Comment 
+                    <textarea className='comments__qj-form__input comments__qj-form__input--comment' name="comment" placeholder='Write your comment here'></textarea>
+                </label>
+                <button className='comments__qj-form__button'>Submit</button>
+            </form>
+        </section>
+    
     )
 }
 
