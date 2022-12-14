bddbdb68f45879fda0a6e4d5bde6705e0b5667db
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-13 20:21:36 -0500

    Finish style and animation for ExerciseTwo component

--- src/components/ExerciseTwo/ExerciseTwo.js
+++ src/components/ExerciseTwo/ExerciseTwo.js
@@ -4,20 +4,28 @@ import {useState} from 'react';
 function ExerciseTwo () {
 
     const [isPlaying, setIsPlaying] = useState(false)
+    const [showText, setShowText] = useState(0)
 
     const handleClick = () => {
         setIsPlaying(true)
+        
+        const timer = setInterval(() => {
+            setShowText(prevIndex => (prevIndex === 0 ? 1 :0));
+        }, 4000);
     }
+
+    const texts = ["Inhale", "Exhale"];
 
 return ( 
 
 
 <section className="breathe2">
     <button className="breathe2__button" onClick={handleClick}>
-        <p className={`breathe2__button--inhale ${isPlaying ? 'in-play' : ""}`}>Inhale</p>
-        <div className={`breathe2__button--circle ${isPlaying ? 'pink' : ""}`} ></div>
-        <div className={`breathe2__button--circle ${isPlaying ? 'turquoise' : ""}`}></div>
-        <p className={`breathe2__button--exhale ${isPlaying ? 'out-play' : ""}`}>Exhale</p>
+        <p className={`breathe2__button--inhale ${isPlaying ? 'in-play' : ""}`}>{texts[showText]}</p>
+        <div className="breathe2__button__circle-container">
+            <div className={`breathe2__button--circle ${isPlaying ? 'pink' : ""}`} ></div>
+            <div className={`breathe2__button--circle ${isPlaying ? 'turquoise' : ""}`}></div>
+        </div>
     </button>
     
 </section>
